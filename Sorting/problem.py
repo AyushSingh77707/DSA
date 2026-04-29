f7376fe6db09e0e7f21a1653951e0c5abe61370b
AyushSingh77707 <ayushsingh77707@gmail.com>
2026-04-29 10:40:17 +0530

    Added Merge sort & quick sort

--- Sorting/problem.py
+++ Sorting/problem.py
@@ -1,3 +1,4 @@
+'''BUBBLE SORT'''
 def bubble_sort(arr):
     n=len(arr)
 
@@ -14,6 +15,7 @@ def bubble_sort(arr):
 nums=[2,5,3,1,10]
 print(bubble_sort(nums))
 
+'''INSERTION SORT'''
 def insertion_sort(arr):
     n=len(arr)
     for i in range(1,n):
@@ -27,6 +29,7 @@ def insertion_sort(arr):
 
 print(insertion_sort(nums))
 
+'''SELECTION SORT'''
 def selection_Sort(arr):
     n=len(arr)
     for i in range(n):
@@ -39,3 +42,67 @@ def selection_Sort(arr):
     return arr
 
 print(selection_Sort(nums))
+
+'''MERGE SORT'''
+def merge(arr,l,mid,r):
+    a=[]
+    b=[]
+    for i in range(l,mid+1):
+        a.append(arr[i])
+    for i in range(mid+1,r+1):
+        b.append(arr[i])
+    i,j,k=0,0,l
+    while k<=r:
+        if j==len(b):
+            arr[k]=a[i]
+            i+=1
+            k+=1
+        elif i==len(a):
+            arr[k]=b[j]
+            j+=1
+            k+=1
+        elif a[i]<b[j]:
+            arr[k]=a[i]
+            i+=1
+            k+=1
+        else:
+            arr[k]=b[j]
+            j+=1
+            k+=1
+def mergesort(arr,l,r):
+    if l>=r:
+        return
+    mid=(l+r)//2
+    mergesort(arr,l,mid)
+    mergesort(arr,mid+1,r)
+    merge(arr,l,mid,r)
+
+def Mergesortarray(arr):
+    mergesort(arr,0,len(arr)-1)
+    return arr
+
+print(Mergesortarray(nums))
+
+'''QUICK SORT'''
+def pivot(arr,l,r):
+    key=arr[r]
+    start=l
+    for i in range(l,r+1):
+        if arr[i]<=key:
+            arr[i],arr[start]=arr[start],arr[i]
+            start+=1
+    return start-1
+
+def quick_sort(arr,l,r):
+    if l>=r:
+        return
+    p=pivot(arr,l,r)
+    quick_sort(arr,l,p-1)
+    quick_sort(arr,p+1,r)
+
+def quicksortarray(arr):
+    quick_sort(arr,0,len(arr)-1)
+    return arr
+
+print(quicksortarray(nums))
+
