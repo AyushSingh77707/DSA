443547b4ee34316397bd93a8f519a7e24f10d23d
AyushSingh77707 <ayushsingh77707@gmail.com>
2026-04-29 09:55:23 +0530

    Added Bubble,insertion,Selection Sorting algorithms

---
+++ Sorting/problem.py
@@ -0,0 +1,41 @@
+def bubble_sort(arr):
+    n=len(arr)
+
+    for i in range(n):
+        swapped=False
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+        if not swapped:
+            break
+    return arr
+
+nums=[2,5,3,1,10]
+print(bubble_sort(nums))
+
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+    return arr
+
+print(insertion_sort(nums))
+
+def selection_Sort(arr):
+    n=len(arr)
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+
+    return arr
+
+print(selection_Sort(nums))
